b814843107e6323aadeed0687a6b4e0a9536ba6c
Olexiy Voedylo <architerr@gmail.com>
2023-02-20 18:00:33 +0200

    ch3

--- les4/task1/index.js
+++ les4/task1/index.js
@@ -1,10 +1,10 @@
-import './polyfills/array-flat';
-import './polyfills/array-flatMap';
+import './polyfills/array-flat.js';
+import './polyfills/array-flatMap.js';
 
 // импортируйте все полифилы из папки polyfills
 import calcExpression, { sum, mult } from './calculator/index.js';
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
-import fetchUser from './polyfills/gateway';
+import fetchUser from './polyfills/gateway.js';
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
 import { printProfile } from './polyfills/index.js';
 
