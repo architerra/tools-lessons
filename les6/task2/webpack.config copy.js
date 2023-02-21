e23fa26aca03d3712219fde18bc9a312e93c6380
Olexiy Voedylo <architerr@gmail.com>
2023-02-21 19:25:59 +0200

    ch2

---
+++ les6/task2/webpack.config copy.js
@@ -0,0 +1,42 @@
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: {
+//       test: /.css$/,
+//       use: ['style-loader', 'css-loader', 'sass-loader'],
+//     },
+//   },
+// };
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              outputPath: 'images',
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
