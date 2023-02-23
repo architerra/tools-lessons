276448855d18ae8ad1dabb9ab1faa1e7add23367
Olexiy Voedylo <architerr@gmail.com>
2023-02-23 15:26:56 +0200

    ch3

--- les7/task2/webpack.config.js
+++ les7/task2/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -32,6 +33,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
