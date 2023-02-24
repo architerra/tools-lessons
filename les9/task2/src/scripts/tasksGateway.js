c5f535a15f6ef52edf847cc394608397f30f5ca9
Olexiy Voedylo <architerr@gmail.com>
2023-02-24 17:35:45 +0200

    ch1

---
+++ les9/task2/src/scripts/tasksGateway.js
@@ -0,0 +1,31 @@
+const baseUrl = 'https://63e0cae2dd7041cafb393784.mockapi.io/api/v1/tasks';
+
+export const createTask = taskData =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  });
+
+export const updateTask = (taskId, taskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  });
+
+export const deleteTask = taskId =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  });
+
+const mapTasks = tasks => tasks.map(({ _id, ...rest }) => ({ id: _id, ...rest }));
+
+export const getTasksList = () =>
+  fetch(baseUrl)
+    .then(responce => responce.json())
+    .then(mapTasks);
---
+++ les9/task2/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { webpack } = require('webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                outputPath: 'images',
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
+
+  return config;
+};
